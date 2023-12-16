3453391f3ce6ee4f5cffa81d25479e1b30a264d2
Luis <luissando326@gmail.com>
2023-12-15 23:32:36 -0600

    Initial commit

--- PY12.py
+++ PY12.py
@@ -1,30 +1,34 @@
+'''
+Luis Sandoval
+'''
+
 import sqlite3
 
-# Connect to the SQLite database
+# Connect
 conn = sqlite3.connect('books.db')
 cursor = conn.cursor()
 
-# Task a: Select all authors’ last names in descending order
+# Task a
 cursor.execute("SELECT last FROM authors ORDER BY last DESC")
 authors_last_names = cursor.fetchall()
 print("Authors' Last Names (Descending Order):")
 for last_name in authors_last_names:
     print(last_name[0])
 
-# Task b: Select all book titles in ascending order
+# Task b
 cursor.execute("SELECT title FROM titles ORDER BY title ASC")
 book_titles = cursor.fetchall()
 print("\nBook Titles (Ascending Order):")
 for title in book_titles:
     print(title[0])
 
-# Task c: INNER JOIN to select books for a specific author
-author_name = "Author's Name"  # Replace with the desired author's name
+# Task c
+author_name = "Wald"
 cursor.execute("""
     SELECT titles.title, titles.copyright, titles.ISBN
     FROM titles
-    INNER JOIN author_ISBN ON titles.ISBN = author_ISBN.ISBN
-    INNER JOIN authors ON author_ISBN.author_id = authors.author_id
+    INNER JOIN author_ISBN ON titles.ISBN = author_ISBN.isbn
+    INNER JOIN authors ON author_ISBN.id = authors.id
     WHERE authors.last = ? 
     ORDER BY titles.title ASC
 """, (author_name,))
@@ -33,17 +37,18 @@ print(f"\nBooks by {author_name} (Alphabetically by Title):")
 for book in author_books:
     print(f"Title: {book[0]}, Copyright: {book[1]}, ISBN: {book[2]}")
 
-# Task d: Insert a new author into the authors table
-new_author = ("New Author's First Name", "New Author's Last Name")
+# Task d
+new_author = ("Luis", "Sandoval")
 cursor.execute("INSERT INTO authors (first, last) VALUES (?, ?)", new_author)
 conn.commit()
 print(f"\nNew author {new_author[0]} {new_author[1]} added to the authors table.")
 
-# Task e: Insert a new title for an author
-new_title = ("New Book Title", 2023, "New Book ISBN")
-cursor.execute("INSERT INTO titles (title, copyright, ISBN) VALUES (?, ?, ?)", new_title)
+# Task e
+new_title = ("Python Experts", 2023, "123456")
+cursor.execute("INSERT INTO titles (isbn, title, edition, copyright) VALUES (?, ?, ?, ?)", (new_title[2], new_title[0], 1, new_title[1]))
+cursor.execute("INSERT INTO author_ISBN (id, isbn) VALUES ((SELECT id FROM authors WHERE first = ? AND last = ?), ?)", (new_author[0], new_author[1], new_title[2]))
 conn.commit()
 print(f"\nNew title '{new_title[0]}' added to the titles table.")
 
-# Close the database connection
 conn.close()
+
